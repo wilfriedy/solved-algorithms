b6e12cd410cb4d02e7589c41b7f0eab3eafd82b6
boydeveloper <wilfriedakpe3@gmail.com>
2022-01-24 12:15:03 +0100

    monday 24th

---
+++ power.js
@@ -0,0 +1,11 @@
+// power
+// Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow()  - do not worry about negative bases and exponents.
+
+const power = (base, expo) => {
+  if (expo === 0) {
+    return 1;
+  }
+  return base * power(base, expo - 1);
+};
+
+console.log(power(2, 3));
---
+++ productOfArray.js
@@ -0,0 +1,7 @@
+// productOfArray
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+const productOfArray = (arr) => {
+  if (arr.length == 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+};
